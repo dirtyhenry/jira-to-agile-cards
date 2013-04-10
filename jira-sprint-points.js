3497733713c2c9d4217507568743fdf4b55610be
Mickaël Floc'hlay <dirtyhenry@gmail.com>
2013-04-10 14:03:21 +0200

    1st draft

---
+++ jira-sprint-points.js
@@ -0,0 +1,35 @@
+totalStoryPoints = 0;
+doneStoryPoints = 0;
+
+issues = document.getElementsByClassName('issuerow');
+for (i = 0; i < issues.length; i++) {  
+    // Find story points & status
+
+    var storyPoints = -1;
+    var status = -1;
+    for (j = 0; j < issues[i].childNodes.length; j++) {
+        if (issues[i].childNodes[j].className == "nav customfield_10105") {
+            storyPoints = parseInt(issues[i].childNodes[j].innerText);
+        } else if (issues[i].childNodes[j].className == "nav status") {
+            status = issues[i].childNodes[j].innerText;
+        }
+    }
+
+    if (storyPoints != -1 && status != -1) {
+        console.log("Adding " + storyPoints);
+        if (status.indexOf("Resolved") > -1 || status.indexOf("Closed") > -1) {
+            doneStoryPoints += storyPoints;
+        } else {
+            console.log("Status: " + status);
+        }
+        if (!isNaN(storyPoints)) {
+            totalStoryPoints += storyPoints;
+        }
+    } else {
+        console.log("An error happened, some fields could not be found.")
+    }
+}
+
+console.log("doneStoryPoints: " + doneStoryPoints);
+console.log("totalStoryPoints: " + totalStoryPoints);
+alert("Done: " + doneStoryPoints + "/" + totalStoryPoints);
